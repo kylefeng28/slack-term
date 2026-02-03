cc824b72703dec27afbea552a90600ca33b412af
Kyle Feng <kylefeng28@gmail.com>
2026-02-03 02:14:06 -0800

    wip bubbletea

--- go.mod
+++ go.mod
@@ -1,21 +1,72 @@
 module github.com/erroneousboat/slack-term
 
-go 1.12
+go 1.24.2
 
 require (
 	github.com/0xAX/notificator v0.0.0-20171022182052-88d57ee9043b
 	github.com/OpenPeeDeeP/xdg v0.2.0
 	github.com/erroneousboat/termui v0.0.0-20170923115141-80f245cdfa04
+	github.com/lithammer/fuzzysearch v1.1.0
+	github.com/mattn/go-runewidth v0.0.19
+	github.com/nsf/termbox-go v0.0.0-20191229070316-58d4fcbce2a7
+	github.com/slack-go/slack v0.6.3
+)
+
+require (
+	github.com/MakeNowJust/heredoc v1.0.0 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/aymanbagabas/go-udiff v0.3.1 // indirect
+	github.com/bits-and-blooms/bitset v1.24.4 // indirect
+	github.com/charmbracelet/bubbles v0.21.1 // indirect
+	github.com/charmbracelet/bubbletea v1.3.10 // indirect
+	github.com/charmbracelet/colorprofile v0.4.1 // indirect
+	github.com/charmbracelet/harmonica v0.2.0 // indirect
+	github.com/charmbracelet/lipgloss v1.1.0 // indirect
+	github.com/charmbracelet/x/ansi v0.11.5 // indirect
+	github.com/charmbracelet/x/cellbuf v0.0.15 // indirect
+	github.com/charmbracelet/x/exp/golden v0.0.0-20241011142426-46044092ad91 // indirect
+	github.com/charmbracelet/x/term v0.2.2 // indirect
+	github.com/clipperhouse/displaywidth v0.9.0 // indirect
+	github.com/clipperhouse/stringish v0.1.1 // indirect
+	github.com/clipperhouse/uax29/v2 v2.5.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
+	github.com/go-test/deep v1.0.4 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
-	github.com/lithammer/fuzzysearch v1.1.0
+	github.com/kr/pty v1.1.1 // indirect
+	github.com/kr/text v0.1.0 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.3.0 // indirect
 	github.com/maruel/panicparse v1.1.1 // indirect
-	github.com/mattn/go-runewidth v0.0.7
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
-	github.com/nsf/termbox-go v0.0.0-20191229070316-58d4fcbce2a7
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/termenv v0.16.0 // indirect
+	github.com/nlopes/slack v0.6.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/slack-go/slack v0.6.3
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/sahilm/fuzzy v0.1.1 // indirect
+	github.com/stretchr/objx v0.1.1 // indirect
 	github.com/stretchr/testify v1.4.0 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	github.com/yuin/goldmark v1.4.13 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
+	golang.org/x/mod v0.13.0 // indirect
+	golang.org/x/net v0.16.0 // indirect
+	golang.org/x/sync v0.4.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/term v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/tools v0.14.0 // indirect
+	golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
--- main.go
+++ main.go
@@ -7,115 +7,221 @@ import (
 	"os"
 
 	"github.com/OpenPeeDeeP/xdg"
-	"github.com/erroneousboat/termui"
-	termbox "github.com/nsf/termbox-go"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/textinput"
+	"github.com/charmbracelet/bubbles/viewport"
+	"github.com/charmbracelet/lipgloss"
 
 	"github.com/erroneousboat/slack-term/context"
-	"github.com/erroneousboat/slack-term/handlers"
 )
 
-const (
-	VERSION = "master"
-	USAGE   = `NAME:
-    slack-term - slack client for your terminal
-
-USAGE:
-    slack-term -config [path-to-config]
-
-VERSION:
-    %s
-
-WEBSITE:
-    https://github.com/erroneousboat/slack-term
-
-GLOBAL OPTIONS:
-   -config [path-to-config-file]
-   -token [slack-token]
-   -cookie [slack-cookie]
-   -debug
-   -help, -h
-`
-)
+const VERSION = "master-bubbletea"
 
 var (
 	flgConfig string
 	flgToken  string
 	flgCookie string
 	flgDebug  bool
-	flgUsage  bool
 )
 
 func init() {
-
-	// Find the default config file
 	configFile := xdg.New("slack-term", "").QueryConfig("config")
-
-	// Parse flags
-	flag.StringVar(
-		&flgConfig,
-		"config",
-		configFile,
-		"location of config file",
-	)
-
-	flag.StringVar(
-		&flgToken,
-		"token",
-		"",
-		"the slack token",
-	)
-
-	flag.StringVar(
-		&flgCookie,
-		"cookie",
-		"",
-		"the slack cookie",
-	)
-
-	flag.BoolVar(
-		&flgDebug,
-		"debug",
-		false,
-		"turn on debugging",
-	)
-
-	flag.Usage = func() {
-		fmt.Printf(USAGE, VERSION)
-	}
-
+	flag.StringVar(&flgConfig, "config", configFile, "location of config file")
+	flag.StringVar(&flgToken, "token", "", "the slack token")
+	flag.StringVar(&flgCookie, "cookie", "", "the slack cookie")
+	flag.BoolVar(&flgDebug, "debug", false, "turn on debugging")
 	flag.Parse()
 }
 
+type model struct {
+	ctx            *context.AppContext
+	channels       list.Model
+	messages       viewport.Model
+	input          textinput.Model
+	currentChannel string
+	ready          bool
+	width          int
+	height         int
+	err            error
+}
+
+func initialModel() (model, error) {
+	usage := "slack-term with bubbletea"
+	ctx, err := context.CreateAppContext(flgConfig, flgToken, flgCookie, flgDebug, VERSION, usage)
+	if err != nil {
+		return model{}, err
+	}
+
+	ti := textinput.New()
+	ti.Placeholder = "Type a message..."
+	ti.Focus()
+
+	return model{
+		ctx:   ctx,
+		input: ti,
+	}, nil
+}
+
+func (m model) Init() tea.Cmd {
+	return tea.Batch(
+		textinput.Blink,
+		loadChannelsCmd(m.ctx),
+	)
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "tab":
+			// Switch focus between channels and input
+			if m.input.Focused() {
+				m.input.Blur()
+			} else {
+				m.input.Focus()
+			}
+		case "enter":
+			if m.input.Focused() && m.input.Value() != "" {
+				// TODO: Send message
+				m.input.SetValue("")
+			} else if !m.input.Focused() {
+				// Load messages for selected channel
+				if item, ok := m.channels.SelectedItem().(channelItem); ok {
+					m.currentChannel = item.id
+					return m, loadMessagesCmd(m.ctx, item.id)
+				}
+			}
+		}
+
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+		if !m.ready {
+			m.channels = list.New([]list.Item{}, list.NewDefaultDelegate(), msg.Width/3, msg.Height-3)
+			m.channels.Title = "Channels"
+			m.messages = viewport.New(msg.Width*2/3, msg.Height-3)
+			m.ready = true
+		} else {
+			m.channels.SetSize(msg.Width/3, msg.Height-3)
+			m.messages.Width = msg.Width*2/3
+			m.messages.Height = msg.Height - 3
+		}
+
+	case channelsLoadedMsg:
+		items := make([]list.Item, len(msg.channels))
+		for i, ch := range msg.channels {
+			items[i] = channelItem{id: ch.ID, name: ch.Name}
+		}
+		m.channels.SetItems(items)
+
+	case messagesLoadedMsg:
+		m.messages.SetContent(msg.content)
+
+	case errMsg:
+		m.err = msg.err
+	}
+
+	var cmd tea.Cmd
+	if m.input.Focused() {
+		m.input, cmd = m.input.Update(msg)
+	} else {
+		m.channels, cmd = m.channels.Update(msg)
+	}
+	return m, cmd
+}
+
+func (m model) View() string {
+	if !m.ready {
+		return "Loading..."
+	}
+
+	channelsView := lipgloss.NewStyle().
+		Width(m.width / 3).
+		Height(m.height - 3).
+		Render(m.channels.View())
+
+	messagesView := lipgloss.NewStyle().
+		Width(m.width*2/3).
+		Height(m.height - 3).
+		Render(m.messages.View())
+
+	inputView := m.input.View()
+
+	main := lipgloss.JoinHorizontal(lipgloss.Top, channelsView, messagesView)
+	return lipgloss.JoinVertical(lipgloss.Left, main, inputView)
+}
+
+type channelItem struct {
+	id   string
+	name string
+}
+
+func (c channelItem) Title() string       { return c.name }
+func (c channelItem) Description() string { return c.id }
+func (c channelItem) FilterValue() string { return c.name }
+
+type channelsLoadedMsg struct {
+	channels []channelData
+}
+
+type messagesLoadedMsg struct {
+	content string
+}
+
+type errMsg struct {
+	err error
+}
+
+type channelData struct {
+	ID   string
+	Name string
+}
+
+func loadChannelsCmd(ctx *context.AppContext) tea.Cmd {
+	return func() tea.Msg {
+		channels := []channelData{}
+		for _, ch := range ctx.Service.Conversations {
+			channels = append(channels, channelData{
+				ID:   ch.ID,
+				Name: ch.Name,
+			})
+		}
+		return channelsLoadedMsg{channels: channels}
+	}
+}
+
+func loadMessagesCmd(ctx *context.AppContext, channelID string) tea.Cmd {
+	return func() tea.Msg {
+		messages, _, err := ctx.Service.GetMessages(channelID, 100)
+		if err != nil {
+			return errMsg{err: err}
+		}
+
+		content := ""
+		for i := len(messages) - 1; i >= 0; i-- {
+			msg := messages[i]
+			content += fmt.Sprintf("%s %s: %s\n", 
+				msg.Time.Format("15:04"), 
+				msg.Name, 
+				msg.Content)
+		}
+
+		return messagesLoadedMsg{content: content}
+	}
+}
+
 func main() {
-	// Start terminal user interface
-	err := termui.Init()
+	m, err := initialModel()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer termui.Close()
 
-	// Create custom event stream for termui because
-	// termui's one has data race conditions with its
-	// event handling. We're circumventing it here until
-	// it has been fixed.
-	customEvtStream := &termui.EvtStream{
-		Handlers: make(map[string]func(termui.Event)),
+	p := tea.NewProgram(m, tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
 	}
-	termui.DefaultEvtStream = customEvtStream
-
-	// Create context
-	usage := fmt.Sprintf(USAGE, VERSION)
-	ctx, err := context.CreateAppContext(
-		flgConfig, flgToken, flgCookie, flgDebug, VERSION, usage,
-	)
-	if err != nil {
-		termbox.Close()
-		log.Println(err)
-		os.Exit(0)
-	}
-
-	// Initialize handlers
-	handlers.Initialize(ctx)
-
-	termui.Loop()
 }
